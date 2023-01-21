8303db1eed0e0c429f23b7be1f7b20b8c9a042e7
fordawin <104473328+fordawin@users.noreply.github.com>
2023-01-21 23:45:07 +0800

    Update for_email.py

--- for_email.py
+++ for_email.py
@@ -16,8 +16,8 @@ conf = ConnectionConfig(
     MAIL_FROM = config_credentials["EMAIL"],
     MAIL_PORT = 587,
     MAIL_SERVER = "smtp.gmail.com",
-    MAIL_TLS = True,
-    MAIL_SSL = False,
+    MAIL_STARTTLS = True,
+    MAIL_SSL_TLS = True,
     USE_CREDENTIALS = True
 )
 
