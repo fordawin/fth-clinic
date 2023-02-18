4c97440cec5bdf90ca8f772d3813f56e9d97dbae
fordawin <104473328+fordawin@users.noreply.github.com>
2023-02-18 19:05:41 +0800

    test

--- database.py
+++ database.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = os.environ.get('DB_URL')
+# SQLALCHEMY_DATABASE_URL = os.environ.get('DB_URL')
 
-# SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5432/fth-db"
+SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5432/fth-db"
 # SQLALCHEMY_DATABASE_URL = "postgresql+psycopg2://postgres:password@localhost:5432/clinicdb"
 # SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root@localhost/clinicdb"
 
